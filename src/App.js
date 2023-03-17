1bb81db1cc6e9ae5d5c9c60e828131795d569623
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-17 17:25:00 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="App"></div>
-    </BrowserRouter>
+    <div></div>
+    // <BrowserRouter>
+    //   <div className="App"></div>
+    // </BrowserRouter>
   )
 }
