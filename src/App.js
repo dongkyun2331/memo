d544f6e1679bf28d75d876239c4773a2b12c162c
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-03-19 23:26:47 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <div></div>
-    // <BrowserRouter>
-    //   <div className="App"></div>
-    // </BrowserRouter>
-  )
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,18 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <div className="header"></div>
+  return (
+    <div className="header">
+      <Link to={"/"}>
+        <h1>
+          <img
+            src="https://memo.naver.com/favicon.ico?762862db-170b-49fa-8ba2-5fbf123d34df"
+            alt=""
+          />
+          네이버 메모
+        </h1>
+      </Link>
+    </div>
+  );
 }
