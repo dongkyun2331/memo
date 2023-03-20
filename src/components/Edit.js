d75a167042cae3cc1852c869382f963911c07d2a
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-03-20 22:06:57 +0900

    a

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,59 +1,45 @@
-import { useRef } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { db } from '../firebase'
-import { doc, updateDoc } from 'firebase/firestore'
+import { useRef } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { db } from "../firebase";
+import { doc, updateDoc } from "firebase/firestore";
 
 export default function Edit() {
-  const titleRef = useRef()
-  const detailRef = useRef()
-  const note = useLocation().state
-  const navigte = useNavigate()
+  const detailRef = useRef();
+  const note = useLocation().state;
+  const navigte = useNavigate();
 
   async function onEdit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    const noteRef = doc(db, 'notes', note.id)
+    const noteRef = doc(db, "notes", note.id);
     await updateDoc(noteRef, {
-      title: titleRef.current.value,
       detail: detailRef.current.value,
-    })
+    });
 
-    navigte('/')
+    navigte("/");
   }
 
   function goBack(e) {
-    e.preventDefault()
-    navigte(-1)
+    e.preventDefault();
+    navigte(-1);
   }
 
   return (
     <div className="container">
-      <h2>노트 수정</h2>
+      <div className="buttons">
+        <button onClick={onEdit}>저장</button>
+        <button onClick={goBack}>취소</button>
+      </div>
       <form>
         <div>
-          <label>제목</label>
-          <input
-            type="text"
-            placeholder="제목"
-            ref={titleRef}
-            defaultValue={note.title}
-          />
-        </div>
-        <div>
-          <label>내용</label>
           <textarea
             cols="30"
             rows="10"
-            placeholder="내용"
             ref={detailRef}
             defaultValue={note.detail}
           ></textarea>
         </div>
-        <div>
-          <button onClick={goBack}>취소하기</button>
-          <button onClick={onEdit}>작성하기</button>
-        </div>
       </form>
     </div>
-  )
+  );
 }
--- src/components/Write.js
+++ src/components/Write.js
@@ -1,52 +1,39 @@
-import { useRef } from 'react'
-import 'moment/locale/ko'
-import { useNavigate } from 'react-router-dom'
-import { db } from '../firebase'
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
+import { useRef } from "react";
+import "moment/locale/ko";
+import { useNavigate } from "react-router-dom";
+import { db } from "../firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 
 export default function Write() {
-  const navigate = useNavigate()
-  const titleRef = useRef()
-  const detailRef = useRef()
+  const navigate = useNavigate();
+  const detailRef = useRef();
 
   async function onWrite(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    const docRef = await addDoc(collection(db, 'notes'), {
-      title: titleRef.current.value,
+    const docRef = await addDoc(collection(db, "notes"), {
       detail: detailRef.current.value,
       date: serverTimestamp(),
-    })
-    navigate('/')
+    });
+    navigate("/");
   }
 
   function goBack(e) {
-    e.preventDefault()
-    navigate(-1)
+    e.preventDefault();
+    navigate(-1);
   }
 
   return (
     <div className="container">
-      <h2>노트 작성</h2>
+      <div className="buttons">
+        <button onClick={onWrite}>저장</button>
+        <button onClick={goBack}>취소</button>
+      </div>
       <form>
         <div>
-          <label>제목</label>
-          <input type="text" placeholder="제목" ref={titleRef} />
-        </div>
-        <div>
-          <label>내용</label>
-          <textarea
-            cols="30"
-            rows="10"
-            placeholder="내용"
-            ref={detailRef}
-          ></textarea>
-        </div>
-        <div>
-          <button onClick={goBack}>취소하기</button>
-          <button onClick={onWrite}>작성하기</button>
+          <textarea cols="30" rows="10" ref={detailRef}></textarea>
         </div>
       </form>
     </div>
-  )
+  );
 }
