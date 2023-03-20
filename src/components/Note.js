fd85724f83d6c6d5245c2a613249e2d7fc9309eb
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-20 15:29:14 +0900

    a

--- src/components/Note.js
+++ src/components/Note.js
@@ -2,9 +2,18 @@ import { useState } from 'react'
 import { MdMode, MdDelete } from 'react-icon/md'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
+import 'moment/locale/ko'
+import { db } from '../firebase'
+import { doc, deleteDoc } from 'firebase/firestore'
 
 export default function Note({ note }) {
-  const [screen, setScreen] = useState(false)
+  const date = moment(note.date.toDate()).format('YYYY-MM-D h:mm')
 
-  return <div>Note</div>
+  async function onDelete() {
+    if (window.confirm('정말 삭제하시겠습니까')) {
+      await deleteDoc(doc(db, 'notes', note.id))
+    }
+  }
+
+  return <li className="note"></li>
 }
