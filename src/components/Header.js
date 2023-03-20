a0eff0934cc6bd2646998b824698ec9764219201
dongkyun2331 <dongkyun2331@gmail.com>
2023-03-20 13:48:31 +0900

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,18 @@ import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <div className="header">
-      <Link to={'/'}>
-        <h1>
-          <img
-            src="https://memo.naver.com/favicon.ico?762862db-170b-49fa-8ba2-5fbf123d34df"
-            alt=""
-          />
-          메모
-        </h1>
-      </Link>
+      <div className="top">
+        <Link to={'/'}>
+          <h1>
+            <img
+              src="https://memo.naver.com/favicon.ico?762862db-170b-49fa-8ba2-5fbf123d34df"
+              alt=""
+            />
+            메모
+          </h1>
+        </Link>
+      </div>
+      <div className="left"></div>
     </div>
   )
 }
